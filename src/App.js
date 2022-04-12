9dd2a8e8952c9264006955e67b3181dc5d56459e
sameerthecoder94 <sameerthecoder@gmail.com>
2022-04-11 20:57:58 -0400

    header & utils added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
+import { getNewExpirationTime, generateId } from './utils/utils';
 
 const App = () => {
+  const [thoughts, setThoughts] = useState([
+    {
+      id: generateId(),
+      text: 'This is a place for your passing thoughts.',
+      expiresAt: getNewExpirationTime(),
+    },
+    {
+      id: generateId(),
+      text: "They'll be removed after 15 seconds.",
+      expiresAt: getNewExpirationTime(),
+    },
+  ]);
+
   return (
-    <div>
-      <h1>React Testing</h1>
+    <div className='App'>
+      <header>
+        <h1>Passing Thoughts</h1>
+      </header>
     </div>
   );
 };
